89192d7fd99e3e26ff88969e0f5d6bcba7c04cbc
Roman Kisliy <omg.dnepr@gmail.com>
2020-02-07 11:59:54 +0200

    Добавил double

--- sandbox/src/main/java/stqa/sandbox/MyFirst.java
+++ sandbox/src/main/java/stqa/sandbox/MyFirst.java
@@ -3,11 +3,16 @@ package stqa.sandbox;
 public class MyFirst {
 
     public static void main(String[] args){
-        System.out.println("Hello," + " world!");
+        String somebody = "world";
+        System.out.println("Hello," + somebody + "!");
 
         int l = 8;
         int s = l*l;
         System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+
+        double a = 8.0;
+        double b = l*l;
+        System.out.println("Площадь квадрата со стороной " + a + " = " + b);
     }
 
 }
