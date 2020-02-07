2640a9520423dac519109c26e60aa1986bc48b69
Roman Kisliy <omg.dnepr@gmail.com>
2020-02-07 11:50:57 +0200

    Пример с площадью квадрата

--- sandbox/src/main/java/stqa/sandbox/MyFirst.java
+++ sandbox/src/main/java/stqa/sandbox/MyFirst.java
@@ -3,7 +3,11 @@ package stqa.sandbox;
 public class MyFirst {
 
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        System.out.println("Hello," + " world!");
+
+        int l = 8;
+        int s = l*l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 
 }
