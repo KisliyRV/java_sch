bb9376755699cb45caf7ac66e1142189a1a51f90
Roman Kisliy <omg.dnepr@gmail.com>
2020-02-11 12:36:44 +0200

    Задание 2.

--- sandbox/src/main/java/stqa/sandbox/Main.java
+++ sandbox/src/main/java/stqa/sandbox/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
 
         PointTwo p1 = new PointTwo(2, 5);
-        PointTwo p2 = new PointTwo(4.0, 8.0);
+        PointTwo p2 = new PointTwo(4, 8);
         System.out.println(p1.p1 + " + " + p2.p2 + " = " + distanceTwo(p1, p2));
 
     }
