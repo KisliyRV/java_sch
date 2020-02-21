cb7e0b7c6d6a72d2cb37d6f4f6d2396cbd08549b
Roman Kisliy <omg.dnepr@gmail.com>
2020-02-21 18:36:50 +0200

    Доработал тесты для удаления и модификации контактов

--- addressbook-web-tests/src/test/java/stqa/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/tests/ContactModificationTest.java
@@ -8,6 +8,9 @@ public class ContactModificationTest extends TestBase {
     @Test
     public void testContactModification() {
         app.getContactHelper().homeContact();
+        if (! app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new AddressData("Dima", "Pupkin", "Test st. 11", "1112223334444", "test55@gmail.com", "2001", "15", "August", "test1"));
+        }
         app.getContactHelper().editContact();
         app.getContactHelper().fillNewContact(new AddressData("Tolik", "Pult", "ot st. 33", "555000", "test101@gmail.com", "1989", "4", "February", null), false);
         app.getContactHelper().updateContact();
