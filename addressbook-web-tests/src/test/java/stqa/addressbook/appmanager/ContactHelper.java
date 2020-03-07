ca1d26a279a20e08c0892f33dcd0e6692ac2d3c8
Roman Kisliy <omg.dnepr@gmail.com>
2020-03-07 11:54:20 +0200

    Добавил проверку на телефон, аддрес и почту.

--- addressbook-web-tests/src/test/java/stqa/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/appmanager/ContactHelper.java
@@ -106,10 +106,12 @@ public class ContactHelper extends HelperBase {
             List<WebElement> cells = element.findElements(By.tagName("td"));
             String firstname = cells.get(2).getText();
             String lastname = cells.get(1).getText();
-           // String mobilephone = cells.get(3).getText();
-           // String email = cells.get(4).getText();
-            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            contacts.add(new AddressData().withId(id).withFirstName(firstname).withLastName(lastname));
+            String mobilephone = cells.get(5).getText();
+            String addresses = cells.get(3).getText();
+            String email = cells.get(4).getText();
+            int id = Integer.parseInt(cells.get(0).findElement(By.tagName("input")).getAttribute("value"));
+            contacts.add(new AddressData().withId(id).withFirstName(firstname).withLastName(lastname)
+                    .withMobilePhone(mobilephone).withAddress(addresses).withEmail(email));
         }
         return contacts;
     }
