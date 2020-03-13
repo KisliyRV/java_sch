816cfd04c9cbca59d8350cd343775616ae3a8860
Roman Kisliy <omg.dnepr@gmail.com>
2020-03-13 17:39:02 +0200

    6.3/6.4
    Создал в тесте массив + доработал генераторы.

--- addressbook-web-tests/src/test/java/stqa/addressbook/tests/GroupCreateTests.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/tests/GroupCreateTests.java
@@ -3,18 +3,31 @@ package stqa.addressbook.tests;
 import org.testng.annotations.*;
 import stqa.addressbook.model.GroupData;
 import stqa.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupCreateTests extends TestBase {
 
-  @Test
-  public void testGroupCreate() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new GroupData().withName("Test1").withHeader("header 1").withFooter("footer1")});
+    list.add(new Object[]{new GroupData().withName("Test1").withHeader("header 1").withFooter("footer1")});
+    list.add(new Object[]{new GroupData().withName("Test1").withHeader("header 1").withFooter("footer1")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreate(GroupData group) {
     app.goTo().groupPage(); //Переходим на страницу группы
     Groups before = app.group().all();
     app.group().creation(); //Создаем тестовую группу
-    GroupData group = new GroupData().withName("test1");
     app.group().fillForm(group); //Заполняем тестовую группу
     app.group().submit(); //применяем изменения
     app.goTo().groupPage();
@@ -25,7 +38,7 @@ public class GroupCreateTests extends TestBase {
   }
 
   @Test
-  public void testFallGroupCreation() throws Exception {
+  public void testFallGroupCreation() {
     app.goTo().groupPage();
     Groups before = app.group().all();
     GroupData group = new GroupData().withName("test'");
